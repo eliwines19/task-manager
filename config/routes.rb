984ff3c0941570cc1ed11b59cd814b156841aa30
eliwines19 <wines.elijah@gmails.com>
2020-07-17 15:57:14 -0400

    added routes and errors layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
-    root "sessions#home"
+  root "sessions#home"
 
-    get '/signup' => 'users#new'
-    post '/signup' => 'users#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
 
-    #login route
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 
-    #logout route
-    delete '/logout' => 'sessions#destroy'
+  #logout route
+  delete '/logout' => 'sessions#destroy'
 
-    #omniauth callback route
-    get "/auth/:provider/callback" => 'sessions#google'
+  #omniauth callback route
+  get "/auth/:provider/callback" => 'sessions#google'
 
-    resources :projects do
-      resources :tasks
-    end
-
+  resources :projects do
     resources :tasks
+  end
 
-    resources :users do
-      resources :projects, shallow: true
-    end
-    
+  resources :tasks
+
+  resources :users do
+    resources :projects, shallow: true
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
