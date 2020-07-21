468d873ca7c4d3185c35ab704a20c253150aec97
eliwines19 <wines.elijah@gmails.com>
2020-07-21 17:00:41 -0400

    started omniauth process

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry'
+
+gem 'omniauth'
+gem 'dotenv-rails'
+gem 'omniauth-facebook'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,23 +16,4 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
 
-    def google
-            #find_or_create a user using the attributes auth
-    @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
-        user.username = auth["info"]["first_name"]
-        user.password = SecureRandom.hex(10)
-    end
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      else
-        redirect_to '/'
-      end
-    end
-
-    private 
-
-    def auth 
-        request.env['omniauth.auth']
-    end
 end
