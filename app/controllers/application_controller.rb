ab7b22470bf8e8cb2ce91d705e1bf6bfee05d579
eliwines19 <wines.elijah@gmails.com>
2020-07-24 10:26:21 -0400

    moved 'pretty_date' to app controller instead of helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     #gives access to methods in the views
-    helper_method :current_user, :logged_in?
+    helper_method :current_user, :logged_in?, :pretty_date
 
 
     private
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::Base
     def redirect_if_not_logged_in
         redirect_to '/' if !logged_in?
     end
+
+    def pretty_date(date)
+        date.strftime(" %B %d, %Y ")
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-
-    def pretty_date(date)
-        date.strftime(" %B - %d - %Y ")
-    end
-
 end
