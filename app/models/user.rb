715447eb2181ce3ff25dd7569874401f3f49b8c8
Elijah Wines <wines.elijah@gmail.com>
2020-12-15 08:49:04 -0500

    projects belong to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :tasks 
+    has_many :projects
+    has_many :tasks
 
     validates :name, presence: true 
     validates :email, presence: true
